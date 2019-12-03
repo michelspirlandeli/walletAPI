b5290045ab64245acc22c53867cbb220c2d39d8b
michelspirlandeli <michelspirlandeli10@gmail.com>
2019-12-03 18:36:32 -0300

    addtest

--- src/test/java/com/wallet/HelloWorld.java
+++ src/test/java/com/wallet/HelloWorld.java
@@ -1,18 +1,18 @@
 package com.wallet;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
+public class HelloWorld {
 
 	@Test
-	public void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
-
 }
