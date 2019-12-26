97a96ff3345ddfc64eddd1d7e1da41068810f5db
michelspirlandeli <michelspirlandeli10@gmail.com>
2019-12-26 20:45:14 -0300

    add entity and repository

---
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.wallet.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wallet.entity.User;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+	@Autowired
+	UserRepository repository;
+	
+	@Test
+	public void testSave() {
+		User u = new User();
+		u.setName("Teste");
+		u.setPassword("123456");
+		u.setEmail("teste@teste.com");
+		
+		User response = repository.save(u);
+		
+		assertNotNull(response);
+	}
+}
