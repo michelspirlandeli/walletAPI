84ee214b04e68dd69818055c1192393df230b085
michelspirlandeli <michelspirlandeli10@gmail.com>
2019-12-31 14:20:25 -0300

    finalizado o repositorio do usuario

--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.wallet.repository;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +21,27 @@ import com.wallet.entity.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserRepositoryTest {
+	
+	private static final String EMAIL = "email@gmail.com";
 
 	@Autowired
 	UserRepository repository;
+	
+	@Before
+	public void setUp() {
+		User u = new User();
+		u.setName("Set up User");
+		u.setPassword("senha123");
+		u.setEmail(EMAIL);
+		
+		repository.save(u);
+	}
+	
+	@After
+	public void tearDown() {
+		
+		repository.deleteAll();
+	}
 	
 	@Test
 	public void testSave() {
@@ -29,5 +53,12 @@ public class UserRepositoryTest {
 		User response = repository.save(u);
 		
 		assertNotNull(response);
+	}
+	
+	public void testFindByEmail() {
+		Optional<User> response = repository.findByEmailEquals(EMAIL);
+		
+		assertTrue(response.isPresent());
+		assertEquals(response.get().getEmail(), EMAIL);
 	}
 }
